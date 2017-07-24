4aa471f77d844c1020c4263f9b62e1c31ac74eee
Chirag Chamoli <chirax@gmail.com>
2017-07-24 07:21:52 +0530

    data for new is finished.

--- static/js/newsdata.js
+++ static/js/newsdata.js
@@ -3,14 +3,183 @@
 
 
 window.getNewsList = function(){
-    return [
-		{
-            "tweet": "RT @myvotetoday: Did you know that the new #AndhraPradesh Capital Region being built at #Amravati by @ncbn Govt will be one of the best cit\u2026",
-            handle: "@SouzaPathrose",
-            "timestamp": moment("Thu Aug 18 16:42:19 +0000 2016").fromNow()
-        },
-        
-    ];
+    return[
+	{
+		"timestamp": "23 Jul 2017, 1:00 pm",
+		"title": "Save The Girl Child, But Tax Her For Being A Girl: The GST Logic",
+		"url": "https://www.youthkiawaaz.com/2017/07/sex-can-be-choice-marriage-can-be-choice-but-periods-is-not-a-choice/",
+		"source" : "youthkiawaaz.com",
+		"summary": "At the midnight hour of July 1, 2017, the then President Pranab Mukherjee and Prime Minister Narendra Modi launched the Goods and Services Tax (GST) at the historic central hall of the Parliament. The new tax was to simplify the tax system, get rid of a chain of different indirect taxes, enhancing the ease of doing business by creating a national market system across India. This was believed to be India’s biggest tax reform since independence. But the government missed some reforms that could have been made for the women of new India. The government had the chance to completely reform the taxation system of India. They should have made sure that the essential goods and services are not taxed.",
+		"krank" : 7,
+		"photo_url": "NA",
+		"author": "Shashi Kumar"
+	},
+	{
+		"timestamp": "23 Jul 2017, 10:30 am",
+		"title": "Of Chester Bennington And Social Media Grieving",
+		"url": "https://www.youthkiawaaz.com/2017/07/grief-in-the-digital-age/",
+		"source" : "youthkiawaaz.com",
+		"summary": "For the past few days, grief and loss have been recurring themes in most of the things I’ve read. I read Paul Kalanithi’s memoir, “When Breath Becomes Air”. Paul was a neurosurgeon who was diagnosed with a life-threatening lung tumour which unfortunately claimed his life in 2015. His memoir shares his desire to leave behind a legacy in the form of this book. The book deals with man’s never-ending quest to find purpose in life, particularly when faced with mortality. His wife, Lucy, shares how she struggles to make sense of her and her daughter’s life after this tragic setback. It was a sombre read, the kind that makes one realize how our perspectives and wishes change as we begin to understand that the years we once thought we had are now mere months or weeks.",
+		"krank" : 2,
+		"photo_url": "https://dz01iyojmxk8t.cloudfront.net/wp-content/uploads/2017/07/23152226/GettyImages-687020100-min.jpg",
+		"author": "Chaitanya Sethi"
+	},
+	{
+		"timestamp": "23 Jul 2017, 6:30 am",
+		"title": "What India Needs To Do To Revive Kashmir’s Dying Economy",
+		"url": "https://www.youthkiawaaz.com/2017/07/kashmirs-staggering-economy-the-road-ahead/",
+		"source" : "youthkiawaaz.com",
+		"summary": "In newspapers and various forms of electronic and social media, we get a lot of news on Kashmir. There is no dearth of perspective among the common strata of the society about this region of conflict and controversy, which has been at the heart of a long standing dispute since 1947 between India and Pakistan and more recently also involving China. While there have been huge discussions, deliberations and debates regarding the presence and role of the Indian Armed Forces and the AFSPA (Armed Forces Special Powers Act ), very little has been spoken about the Kashmiri economy of the pre conflict era, its stagnation and overdependence on the center.",
+		"krank" : 5.5,
+		"photo_url": "NA",
+		"author": "Namit Bhasin"
+	},
+	{
+		"timestamp": "23 Aug 2012, 3:25 am",
+		"title": "Problem: Elections! Solution: Emergency 2012!",
+		"url": "http://www.mediacrooks.com/2012/08/problem-elections-solution-emergency.html",
+		"source" : "mediacrooks.com",
+		"summary": "This is from the New YorkTimes: “The quotations come back redacted, stripped of colorful metaphors, colloquial language and anything even mildly provocative. They are sent by e-mail from the Obama headquarters in Chicago to reporters who have interviewed campaign officials under one major condition: the press office has veto power over what statements can be quoted and attributed by name”. In other words any quote from the Obama campaign that appears in media is pre-censored by his press office. Out in the US they are now calling it “quote approval” or “message approval”. In India the govt and the media are creating “phenomenon and semantics” to create fear and through it muzzle free speech. The phenomenon here is to use the Assam violence and subsequent events to create fear and panic and distract from many other real issues. The semantics: Spreading hate, images inciting violence, hate-speech and communal threats! And all of this mainly coming from only one source: Social media on the internet.",
+		"krank" : 6,
+		"photo_url": "http://4.bp.blogspot.com/-0JLXSf_RLbw/UDWiBLmtdbI/AAAAAAAAAu0/m7uhAsiX9Uo/s320/BlockedSites.jpg",
+		"author": "Ravinar"
+	},	{
+		"timestamp": "19 Sep 2016, 9:57 am",
+		"title": "Open Letter to PM Modi",
+		"url": "http://amreekandesi.com/2016/09/19/open-letter-to-pm-modi/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+amreekandesi+%28amreekandesi.com%29",
+		"source" : "amreekandesi.com",
+		"summary": "Back in 2014 when India overwhelmingly voted for you, we voted in the belief that the aspirations of modern India would now be met. That we would finally get away from the platitudes of ‘we want to remove poverty’ while ensuring that the poor remained so. That the long-ignored middle class would finally get heard. That we won’t be ignored as a useless clump of 500-600 million people that don’t belong to a clear cut vote-bank, and are thus irrelevant in the democratic scheme of things. That India will finally become a country that values merit, instead of caste and community.",
+		"krank" : 4.5,
+		"photo_url": "http://i0.wp.com/amreekandesi.com/wp-content/uploads/2016/09/modi-new-759.jpg?w=759",
+		"author": "Atulya Mahajan"
+	},	{
+		"timestamp": "1 Jun 2017, 10:43 am",
+		"title": "Banning the Sale of Cattle for Slaughter - Government has no role in Kitchen ",
+		"url": "http://www.searchforanidentity.com/2017/06/banning-sale-of-cattle-for-slaughter.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+sajeevk+%28Astitva+-+Search+for+an+Identity%29",
+		"source" : "searchforanidentity.com",
+		"summary": "The notification in question is, ‘Prevention of Cruelty to Animals (Regulation of Livestock Markets) Rules, 2017' from Ministry of Environment and Forests. As the name implies this rule have many progressive directions like,",
+		"krank" : 6,
+		"photo_url": "https://2.bp.blogspot.com/-Hry5DD81uec/WS_vewmqI9I/AAAAAAAADyU/6ERYAhTj5nscyWylOHr71ilqXVOqFyCCgCLcB/s640/Raw_beef_slices.jpg",
+		"author": "Sajeev"
+	},	{
+		"timestamp": "1 May 2017, 7:45 am",
+		"title": "Frequently Asked Questions on GST (Goods and Services Tax) - Part I",
+		"url": "http://www.searchforanidentity.com/2017/04/frequently-asked-questions-on-gst-goods.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+sajeevk+%28Astitva+-+Search+for+an+Identity%29",
+		"source" : "searchforanidentity.com",
+		"summary": "This Question and Answers are prepared by Apex Training Institute Under Central Board of Excise and Customs(CBEC) with some inputs from National Academy of Customs, Excise and Narcotics (NACEN).",
+		"krank" : 7,
+		"photo_url": "https://1.bp.blogspot.com/-KRAdmJnz2AI/WQbniPiwL2I/AAAAAAAADxc/LKpfuQAb0_ktgrysLxH_evMOhrNsHF7iQCLcB/s320/Sarnath_Lion_Capital_of_Ashoka.jpg",
+		"author": "Sajeev"
+	},	{
+		"timestamp": "22 Jul 2017, 9:42 pm",
+		"title": "TN farmers to protest in Delhi, yet again: What does it imply?",
+		"url": "https://theindianeconomist.com/tn-farmers-protest-delhi-yet-imply/",
+		"source" : "theindianeconomist.com",
+		"summary": "Wearing nothing but loincloths, about 40 Tamil Nadu farmers, who had returned to their home state in April after a 41-day agitation, are back in Delhi. This time, they have been joined by their counterparts from Rajasthan, Maharashtra, Punjab, Haryana and Telangana.",
+		"krank" : 6,
+		"photo_url": "https://i0.wp.com/theindianeconomist.com/wp-content/uploads/2017/07/jantar-mantar.jpg?w=1024",
+		"author": "Saarthak Anand"
+	},	{
+		"timestamp": "22 Jul 2017, 2:17 pm",
+		"title": "The Daily Fix: Communal tensions in coastal Karnataka are a failure of leadership and policing",
+		"url": "https://theindianeconomist.com/daily-fix-communal-tensions-coastal-karnataka-failure-leadership-policing/",
+		"source" : "theindianeconomist.com",
+		"summary": "Officials in Karnataka’s Mangaluru imposed prohibitory orders for the next two weeks starting Sunday, making it illegal for crowds to gather in public in the coastal city that has frequently been the site of communal tensions. The orders for Mangaluru come even as prohibitory orders hit the 50-day mark in the taluk of Bantwal, as well as several other taluks, all in the district of Dakshina Kannada. The ostensible reason for Section 144, which prevents unlawful assemblies of more than four people in public, to be imposed are the recent clashes between communities that have flared up and even led to several deaths over the last few months.",
+		"krank" : 7,
+		"photo_url": "https://i2.wp.com/theindianeconomist.com/wp-content/uploads/2017/07/63323-kyzdpdhrvm-1500264498.jpg?w=1200",
+		"author": "NA"
+	},	{
+		"timestamp": "22 Jul 2017, 9:03 am",
+		"title": "New caste war in Uttar Pradesh: 152 of the 312 new law officers appointed by Adityanath are Brahmins ",
+		"url": "https://theindianeconomist.com/new-caste-war-uttar-pradesh-152-312-new-law-officers-appointed-adityanath-brahmins/",
+		"source" : "theindianeconomist.com",
+		"summary": "Nearly half of the 312 law officers appointed by the Adityanath government in Uttar Pradesh on July 7 belong to the Brahmin caste. This is being seen as a bid by the ruling dispensation to assuage the sentiments of Brahmins who are said to be enraged after five of their caste brethren were killed in Rae Bareli last month. The move is also seen as an attempt by Adityanath – who is a Thakur by caste – to correct a perceptible tilt towards Thakurs in government appointments ever since he became chief minister early this year.",
+		"krank" : 7,
+		"photo_url": "https://i1.wp.com/theindianeconomist.com/wp-content/uploads/2017/07/rtanegclpx-1491315719.jpg?w=1200",
+		"author": "Dhirendra K Jha"
+	},	{
+		"timestamp": "22 Jul 2017, 4:57 am",
+		"title": "Karnataka transfers Bengaluru police official who claimed Sasikala got special treatment in jail ",
+		"url": "https://theindianeconomist.com/karnataka-transfers-bengaluru-police-official-claimed-sasikala-got-special-treatment-jail/",
+		"source" : "theindianeconomist.com",
+		"summary": "Karnataka Deputy Inspector General (Prisons) D Roopa, who had alleged that special treatment was being given to a few celebrity inmates in Bengaluru’s Parapanna Agrahara Central Prison, has been transferred, India Today reported on Monday. Among the prisoners named in her report was All India Anna Dravida Munnetra Kazhagam chief VK Sasikala. Jail Superintendent Krishna Kumar has also been transferred. R Anitha will take over the position",
+		"krank" : 5,
+		"photo_url": "https://i2.wp.com/theindianeconomist.com/wp-content/uploads/2017/07/Police-Officer.jpg?w=1200",
+		"author": "NA"
+	},	{
+		"timestamp": "21 Jul 2017, 6:59 pm",
+		"title": "Crunch in the capital: New Delhi is running out of water ",
+		"url": "https://theindianeconomist.com/crunch-in-the-capital-new-delhi-is-running-out-of-water/",
+		"source" : "theindianeconomist.com",
+		"summary": "As summer temperatures soar above 40°C in New Delhi, acute water shortages are gripping parts of India’s capital. Signs of water stress are now everywhere, and residents in southern and western parts of the city have not received a regular, reliable water supply for months.",
+		"krank" : 5,
+		"photo_url": "https://i1.wp.com/theindianeconomist.com/wp-content/uploads/2017/07/1280px-FEMA_-_44898_-_Empty_Lake_Delhi_and_boats_with_no_water.jpg?w=1280",
+		"author": "Asit K. Biswas"
+	},	{
+		"timestamp": "28 Mar 2017, 3:36 am",
+		"title": "Aadhaar: The Promise and Reality of the Surveillance State",
+		"url": "https://aamjanata.com/aadhaar-promise-reality-surveillance-state/",
+		"source" : "aamjanata.com",
+		"summary": "Tighten the chokehold and kill dissent. When anonymity goes away, public debate is more silent. Too much democracy (sounds wierd, I know) and freedom is a bad thing for those in power.",
+		"krank" : 6,
+		"photo_url": "https://aamjanata.com/wp-content/uploads/aadhaar-logo-one-unchangeable-password-everywhere-even-if-leaked-no-ok-reset-your-fingerprints.jpg",
+		"author": "VIDYUT"
+	},	{
+		"timestamp": "19 Jun 2017, 7:56 am",
+		"title": "How Madhya Pradesh farmers agitation is testing CM Shivraj Chouhan's kisan putra image and his agri track record",
+		"url": "http://indiatoday.intoday.in/story/shivraj-singh-chouhan-farmers-agitation-madhya-pradesh/1/979116.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "A week is a long time in politics. Union minister for agriculture Radha Mohan Singh rediscovered the truism the hard way. Just two weeks ago, he told India Today that the central government would like to emulate the Madhya Pradesh model of growth in agriculture. A week later, five farmers in the state were killed in police firing, a sixth died on Friday of injuries sustained during curfew, highways in western MP were cut off, about 150 vehicles were torched, a DM was manhandled and railway lines damaged by agitating farmers.",
+		"krank" : 5,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/shivraj-jun26-1_647_061517055753.jpg",
+		"author": "Rahul Noronha"
+	},	{
+		"timestamp": "9 Jun 2017, 3:05 pm",
+		"title": "Bovine card",
+		"url": "http://indiatoday.intoday.in/story/bovine-cow-beef-ban-bjp-nda-tamil-nadu-iit-madras-kerala-west-bengal/1/973500.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "On May 23, when the BJP-led NDA government issued a notification banning the sale of cattle for slaughter, three states erupted in protest: Kerala, Tamil Nadu and West Bengal. Though the ban includes cow, calf, bull, bullock, buffalo, heifer, steer and camel, the protests centred around the cow, as it is the protection of this animal that the saffron party is consumed with. And the prime mover of this new rule, according to sources, is BJP president Amit Shah, who has been unequivocal about his stand on cow slaughter. 'One of the biggest challenges before India is how to save its cattle wealth from perishing,' Shah has told india today on numerous occasions. 'If cattle wealth decreases, the prosperity of the farmer too decreases.'",
+		"krank" : 7,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/cow-jun19-1_647_060917115816.jpg",
+		"author": "Uday Mahurkar"
+	},	{
+		"timestamp": "28 May 2017, 2:02 am",
+		"title": "IAS officer's conviction sends shockwaves through bureaucracy, threatens to paralyse government",
+		"url": "http://indiatoday.intoday.in/story/h-c-gupta-convicted-corruption-madhya-pradesh/1/962434.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "On May 22, the Central Bureau of Investigation's special court in Delhi sentenced former coal secretary H.C. Gupta and two other serving government officials - K.S. Kropha and K.C. Samaria - to a two-year prison term. Earlier pronounced 'guilty' of wrongdoing in the allocation of a coal block in Madhya Pradesh to a private firm, Kamal Sponge Steel & Power Limited (KSSPL), in 2008, the three were also ordered to pay individual fines of Rs 1 lakh each. Besides the bureaucrats, the court imposed a Rs 1 crore penalty on the company and a personal fine of Rs 30 lakh and three-year jail term for its MD, Pawan Kumar Ahluwalia.",
+		"krank" : 7,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/babu-story-jun5-1_647_052517020641.jpg",
+		"author": "Asit Jolly"
+	},	{
+		"timestamp": "21 Jul 2017, 12:52 pm",
+		"title": "India's competitive parliamentary democracy needs a President, but pomp and Raj legacy worth it?",
+		"url": "http://indiatoday.intoday.in/story/president-india-ram-nath-kovind-dalit-rashtrapati-bhavan-expenditure-white-house/1/1006437.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "In its 70th year of Independence, the government has chosen a Dalit candidate for its highest office of the head of the state for the second time. Ram Nath Kovind, former governor of Bihar, is the second Scheduled Caste presidential candidate after K.R. Narayanan became the country's 10th, and the first Dalit, president in 1997. As democratic politics is first and foremost a symbolic contest for representation, the choice of a Dalit candidate was a move that was widely described as another 'masterstroke' by Prime Minister Narendra Modi. So formidable was the first-mover advantage the NDA gained by selecting a Dalit candidate for the presidency that the Congress's Meira Kumar, also a Dalit, became an obvious choice as candidate for the 17-party mahagathbandhan. However, paradoxically, while choosing candidates from Dalit backgrounds, the leadership across political parties has been silent on the enormous costs the country's taxpayers bear to house and maintain the office of the Presidency.",
+		"krank" : 6,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/president-jul31-1-647_072117043939.jpg",
+		"author": "Ajit Kumar Jha"
+	},	{
+		"timestamp": "17 Jul 2017, 9:41 am",
+		"title": "CBI raids against Lalu Prasad, son Tejashwi and the Mahagathbandhan crisis in Bihar ",
+		"url": "http://indiatoday.intoday.in/story/bihar-lalu-prasad-yadav-cbi-nitish-kumar-rjd/1/1000947.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "'Bring in the crowds at Gandhi Maidan on August 27, as many people as you can,' Lalu Prasad Yadav told his Rashtriya Janata Dal (RJD) legislators, as he sat with them at his 10, Circular Road, residence in Patna on July 10, two days after a 27-member Central Bureau of Investigation (CBI) team raided his premises and seized property documents and laptops.",
+		"krank" : 4,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/lalu-jul24-1_647_071317073334.jpg",
+		"author": "Amitabh Srivastava"
+	},	{
+		"timestamp": "10 Jul 2017, 8:11 am",
+		"title": "Why Nitish Kumar is at the top of the pecking order in Bihar's politics",
+		"url": "http://indiatoday.intoday.in/story/bihar-nitish-kumar-lalu-prasad-yadav-nda-jdu-congress-lok-sabha-2019/1/995392.html",
+		"source" : "indiatoday.intoday.in",
+		"summary": "As Bihar chief minister Nitish Kumar rose to address the state executive committee of his party, the Janata Dal (United), or the JD(U), during a closed-door meeting in Patna on July 2, he was unsparingly critical of the Congress, a junior ally in his grand alliance government with four ministers in the 29-member cabinet.",
+		"krank" : 6,
+		"photo_url": "http://media2.intoday.in/indiatoday/images/stories/nitish-jul17-1_647_070617031008.jpg",
+		"author": "Amitabh Srivastava"
+	}
+]
 }
 
 
