1495bc1acdbd21c5bf61dd611b20005274cd347b
Kishan Pradhan <kishanpradhan100@gmail.com>
2017-11-03 13:43:06 +0530

    Add auth decorator to module

---
+++ app/mod_library/auth.py
@@ -0,0 +1,38 @@
+from flask import request
+
+from app.mod_repository.stocktwist import UserTokenRepo
+
+class Auth():
+    token_user = {}
+
+    @classmethod
+    def user(cls, token=None, for_decorator=False):
+        token = request.headers.get('Authorization') if token is None else token
+        if token in cls.token_user:
+            return cls.token_user[token]
+        user = UserTokenRepo().get_auth_user(token)
+        if token not in cls.token_user and for_decorator:
+            cls.token_user[token] = user
+        return user
+
+    @classmethod
+    def remove_user(cls, token=None):
+        """
+        Have to use expiration date for token_user
+        """
+        token = request.headers.get('Authorization') if token is None else token
+        if token in cls.token_user:
+            del cls.token_user[token]
+        return 
+
+def user(for_decorator=False):
+    return Auth.user(for_decorator=for_decorator)
+
+def auth_required(func):
+
+    def wrap(*args, **kwargs):
+        user(for_decorator=True)
+        result = func(*args, **kwargs)
+        Auth.remove_user()
+        return result
+    return wrap
