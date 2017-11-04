db687ae9697e809edf1822fc94aabdf9430085c7
Kishan Pradhan <kishanpradhan100@gmail.com>
2017-11-04 14:30:08 +0530

    check upload from microservice and add templates for admin

--- app/mod_manage/controllers.py
+++ app/mod_manage/controllers.py
@@ -1,25 +1,54 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, redirect, url_for
-from app.mod_repository.user import UserRepo
-from app.mod_library import router
+
+import views
+from app.mod_library import response
+from app.mod_library.exception import SException
 
 
 # Define the blueprint: 'app', set its url prefix: app.url/auth
 mod_manage = Blueprint('manage', __name__, url_prefix='/manage')
 
 # Set the route and accepted methods
-@mod_manage.route('/')
-def home():
-	return render_template('manage/feeds/home.html')
+@mod_manage.route('/companies')
+def company_list():
+    companies = views.list_companies()
+    return render_template('manage/company/list.html', companies=companies)
 
-#@mod_manage.route('/usersss')
-#@router.api_parent.route('/users')
-@router.api('users', methods=['GET'])
-def users():
-    #print UserRepo().create({'name': 'Kishan', 'email': 'kkl'})
-    #print UserRepo().all()
-    return "USers"
+@mod_manage.route('/companies/<company_id>')
+def companies_admin(company_id):
+    if request.method == 'POST':
+        company = view.update_companies(company_id, request.values)
+    else:
+        company = views.get_company_details(company_id)
+    return render_template('manage/company/details.html', company = company)
 
-@mod_manage.route('/uploads', methods=['POST'])
-def upload():
-    return "upload"
+@mod_manage.route('/users')
+def user_list():
+    users = views.list_users()
+    print users
+    return render_template('manage/user/list.html', users = users)
+
+@mod_manage.route('/users/<user_id>')
+def user_admin(user_id):
+    if request.method == 'POST':
+        user = views.update_user(user_id, request.values)
+    else:
+        user = views.get_user_details(user_id)
+    return render_template('manage/user/details.html', user = user)
+
+@mod_manage.route('/uploads/<upload_id>', methods=['POST'])
+def upload(upload_id):
+    try:
+        if request.json:
+            res = views.update_upload(upload_id, request.json)
+        elif len(request.values.to_dict()):
+            res = views.update_upload(upload_id, request.values.to_dict())
+        else:
+            raise SException('Please provide some arguments', 400)
+        return response.json({'data': 'success'})
+    except SException as e:
+        raise e
+    except:
+        raise SException('Invalid request', 400)
+
--- app/mod_manage/views.py
+++ app/mod_manage/views.py
@@ -1,25 +1,31 @@
 
+from app.mod_repository.stocktwist import CompanyRepo, UserRepo, UploadRepo
+
+def list_companies():
+    return CompanyRepo().objects.paginate(10, 1)
+
+def get_company_details(company_id):
+    return CompanyRepo().filter(id=company_id)
+
+def update_company(company_id, data):
+    return CompanyRepo().filter(id=company_id).update(data)
+
 def create_user(data):
     pass
 
+def list_users():
+    return UserRepo().objects.paginate(1, 10)
+
 def get_user_details(user_id):
-    pass
+    return UserRepo().objects.filter(id=user_id)
 
-def comment_on_company(comp_id, data):
-    pass
+def update_user(user_id, data):
+    return UserRepo().objects.filter(id=user_id).update(data)
 
-def get_all_comments(comp_id):
-    pass
-
-def replay_to_comment(comment_id, data):
-    pass
-
-def get_all_replies(comment_id):
-    pass
-
-def mark_as_favourite(comp_id, user_id):
-    pass
-
-def remove_from_favourites(comp_id, user_id):
-    pass
+def update_upload(upload_id, data):
+    if 'id' in data: del data['id']
+    # if we give 'size' as attr it is giving error so make it '_size'
+    if 'size' in data: data['_size'] = data['size']; del data['size'];
+    print data
+    return UploadRepo().objects.filter(id=upload_id).update(**data)
 
